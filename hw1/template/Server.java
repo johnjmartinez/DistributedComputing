e544a9dfb9c66941bf138251f3c50d42aa2de0c8
triskadecaepyon <dcltechnology@gmail.com>
2015-09-08 22:21:04 -0500

    Working TCP code!!!
    Fixed with a thread running TCP, and handlers with each thread socket.
    Will need to sync the data structure call.

--- hw1/template/Server.java
+++ hw1/template/Server.java
@@ -6,12 +6,11 @@ import java.util.HashMap;
 public class Server {
     // Create Data structure out here that holds the array or data structure of reservations, init on main
     // Accesses to this must be Synchronized
-    HashMap<Integer, String> reservationSystem;
+    SeatingData myseats;
 
-    //Constructor for server
+    //Constructor for server, needs only size for seating data
     public Server(Integer size) {
-        //Is this the right data structure? not sure right now
-        reservationSystem = new HashMap<Integer, String>();
+        this.myseats = new SeatingData(size);
     }
 
     public static void main (String[] args) {
@@ -33,14 +32,15 @@ public class Server {
 
     // TODO: handle request from clients
 
-    Server myServer = new Server(N);
     System.out.println("starting server");
+    //Start servers, including seating server
+    Server myServer = new Server(N);
+    TCPServer myTCP = new TCPServer(tcpPort, myServer.myseats);
+    new Thread(myTCP).start();
 
     // Main loop goes here, while (true)
         try {
             System.out.println(InetAddress.getLocalHost());
-            ServerSocket serverSocket = null;
-            serverSocket = new ServerSocket(tcpPort);
 
             while(true) {
                 DatagramSocket socket = new DatagramSocket(udpPort);
@@ -66,15 +66,6 @@ public class Server {
                 //Parse packet
 
                 socket.close();
-
-//                Socket clientSocket = null;
-//                try {
-//                    clientSocket = serverSocket.accept();
-//                    System.out.println("accepted");
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                new Thread(new HandleRequest(clientSocket, "MultithreadServer")).start();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -103,6 +94,40 @@ public class Server {
     }
 }
 
+class TCPServer implements Runnable {
+    SeatingData seatingData;
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    Integer port;
+    Thread runningThread = null;
+    public TCPServer(Integer tcpport, SeatingData seatserver) {
+        this.seatingData = seatserver;
+        this.port = tcpport;
+    }
+
+    @Override
+    public void run() {
+        synchronized(this){
+            this.runningThread = Thread.currentThread();
+        }
+
+        try {
+            this.serverSocket = new ServerSocket(this.port);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        while(true) {
+            try {
+                this.clientSocket = this.serverSocket.accept();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            new Thread(new HandleRequest(clientSocket, "Multithreaded Server")).start();
+        }
+    }
+}
+
 class HandleRequest implements Runnable{
     Socket clientSocket;
     String serverText = null;
@@ -124,5 +149,14 @@ class HandleRequest implements Runnable{
 
             e.printStackTrace();
         }
+    }
+}
+
+class SeatingData {
+    final Integer seat_allocation;
+    HashMap<Integer, String> reservationSystem;
+
+    public SeatingData(Integer seats) {
+        this.seat_allocation = seats;
     }
 }
